054df28584f58984a7d0d1ed37ae0d1dbdc72795
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-04-21 12:15:55 -0500

    Presentación de contenido en una plantilla

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,8 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 def index():
-    return "¡Hola a todos!"
-
-
-@app.route('/otro-saludo')
-def otro_saludo():
-    return 'Hello!'
-
-
-@app.route('/saludo/<nombre>')
-def saludo(nombre):
-
-    contenido = """
-    <html>
-        <head>
-            <title>Saludo</title>
-        </head>
-        <body>
-            <h1>¡Hola, {}!</h1>
-            <img src="https://pypi.org/static/images/logo-small.8998e9d1.svg" alt="Python Package Index">
-            <br>
-            <a href="https://ortizol.blogspot.com">Blog de John Ortiz</a>
-        </body>
-    </html>
-    """
-    
-    return contenido.format(nombre)
+    return render_template("index.html", titulo='Inicio', nombre='Edward')
