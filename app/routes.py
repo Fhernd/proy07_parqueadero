5bf1349952d4d6946ff86d260297f21d9fe21481
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-04-22 14:41:44 -0500

    Plantilla para saludos dinámicos

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,8 @@ from app import app
 
 @app.route("/")
 def index():
-    return render_template("index.html", titulo='Inicio', nombre='Oliva')
+    return render_template("index.html", titulo='Inicio', nombre='Alex')
+
+@app.route("/hola/<nombre>")
+def hola(nombre):
+    return render_template("hola.html", titulo='Hola', nombre=nombre)
