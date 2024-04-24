5e1b4ac95909212091013c573c8c2545feb8a307
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-04-23 20:53:33 -0500

    Modelo de usuario

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
 from app import routes
