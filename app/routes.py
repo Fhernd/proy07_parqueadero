34f5185770a3fbcbe4dd4da4ef4a39dc3d0ef432
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-04-25 13:30:54 -0500

    Eliminación de funcionese de vista no necesarias

--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,3 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html", titulo='Inicio', nombre='Alex')
-
-@app.route("/hola/<nombre>")
-def hola(nombre):
-    return render_template("hola.html", titulo='Hola', nombre=nombre)
-
-
-@app.route('/lenguajes-programacion')
-def lenguajes_programacion():
-    lenguajes = ['Python', 'Java', 'JavaScript', 'C#', 'PHP', 'Ruby', 'Swift', 'Kotlin']
-    return render_template('lenguajes.html', titulo='Lenguajes de programación', lenguajes=lenguajes)
