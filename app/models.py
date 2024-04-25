237a856eb80a51975c4a19d7699f14fe60b65733
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-04-25 12:56:00 -0500

    Modelo SQLAlchemy para la tabla Parqueadero

--- app/models.py
+++ app/models.py
@@ -14,3 +14,22 @@ class Sede(db.Model):
 
     def __repr__(self):
         return f'<Sede {self.nombre}>'
+
+
+class Parqueadero(db.Model):
+    """
+    Representa un parqueadero.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    rut = db.Column(db.String(32), unique=True, nullable=False)
+    nombre = db.Column(db.String(64), nullable=False)
+    direccion = db.Column(db.String(255), nullable=False)
+    telefono = db.Column(db.String(16), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+    pais_id = db.Column(db.Integer, db.ForeignKey('pais.id'), nullable=False)
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<Parqueadero {self.vehiculo_placa}>'
