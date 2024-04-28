b659a32ab49771f04c65ae25e783f70b0e7cfa69
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-04-28 11:16:54 -0500

    Ruta para la vista de gestión de vehículos

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,7 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html", titulo='Inicio', nombre='Alex')
+
+@app.route("/vehiculo-tipo", methods=['GET'])
+def vehiculo_tipo():
+    return render_template("vehiculo-tipo.html", titulo='Tipo de Vehículo')
