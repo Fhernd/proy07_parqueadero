aa588153e14b505cbd24336e5cd0ec1a7713a4f7
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-06 19:09:12 -0500

    Eliminación de un tipo de vehículo a partir de su ID

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 
-from app import app
+from app import app, db
 
 from app.models import VehiculoTipo
 
@@ -12,6 +12,17 @@ def index():
 @app.route("/vehiculo-tipo", methods=['GET'])
 def vehiculo_tipo():
     tipos_vehiculo = VehiculoTipo.query.all()
-    print(tipos_vehiculo)
-    print(len(tipos_vehiculo))
     return render_template("vehiculo-tipo.html", titulo='Tipo de Vehículo', tipos_vehiculo=tipos_vehiculo)
+
+
+@app.route('/vehiculo-tipo/<int:id>', methods=['DELETE'])
+def vehiculo_tipo_delete(id):
+    vehiculo_tipo = VehiculoTipo.query.get(id)
+    
+    if vehiculo_tipo is None:
+        return 'No existe el tipo de vehículo', 404
+    
+    db.session.delete(vehiculo_tipo)
+    db.session.commit()
+    
+    return 'Tipo de vehículo eliminado', 200
