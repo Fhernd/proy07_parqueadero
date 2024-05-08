77b03c0cdb6c45b3c4f0e66182475b7504688c26
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-07 20:55:45 -0500

    Función de vista para actualizar un tipo de vehículo

--- app/routes.py
+++ app/routes.py
@@ -51,3 +51,32 @@ def vehiculo_tipo_create():
         print(e)
         db.session.rollback()
         return jsonify({'status': 'error', 'message': str(e)}), 500
+
+
+@app.route('/vehiculo-tipo/<int:id>', methods=['PUT'])
+def vehiculo_tipo_update(id):
+    """
+    Actualiza un tipo de vehículo.
+
+    :param id: Identificador del tipo de vehículo.
+    :return: Respuesta JSON.
+    """
+    try:
+        data = request.get_json()
+        vehiculo_tipo = VehiculoTipo.query.get(id)
+
+        if vehiculo_tipo is None:
+            return jsonify({'status': 'failure', 'message': 'Tipo de vehículo encontrado'}), 404
+
+        vehiculo_tipo.nombre = data.get('nombre')
+
+        db.session.commit()
+
+        return jsonify({'status': 'success', 'message': 'Tipo de vehículo actualizado', 'data': {
+            'id': vehiculo_tipo.id,
+            'nombre': vehiculo_tipo.nombre
+        }}), 200
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'status': 'error', 'message': str(e)}), 500
