ee2a82249e5e3a80dcdb60f083a1facaf327a7e3
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-15 18:53:07 -0500

    Modelo usuario con el campo es_propietario

--- app/models.py
+++ app/models.py
@@ -80,6 +80,7 @@ class Usuario(db.Model):
     telefono = db.Column(db.String(16), nullable=False)
     email = db.Column(db.String(64), nullable=False)
     rol_id = db.Column(db.Integer, db.ForeignKey('rol.id'), nullable=False)
+    es_propietario = db.Column(db.Boolean, default=False, nullable=True)
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, server_default=db.func.current_timestamp())
 
