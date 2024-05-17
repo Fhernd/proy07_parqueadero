32ed76af2eb7187978c65afc5eb9747c70f620da
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-17 06:31:41 -0500

    Instanciación de LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 from config import ConfigDevelopment
@@ -7,6 +8,9 @@ app = Flask(__name__)
 app.config.from_object(ConfigDevelopment)
 
 db = SQLAlchemy(app)
+
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import models
 
