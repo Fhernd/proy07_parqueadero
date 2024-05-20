fcf3dfefd1cc4a84f46de0a956545a95cbc3ddcc
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-05-20 06:08:06 -0500

    Formulario para los datos del usuario que se mostrarán en su perfil

---
+++ app/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from models import Usuario  # Importa tu modelo de usuario
+
+class UsuarioForm(FlaskForm):
+    documento = StringField('Documento', validators=[DataRequired(), Length(max=16)])
+    password = PasswordField('Contraseña', validators=[DataRequired(), Length(max=256)])
+    nombres = StringField('Nombres', validators=[DataRequired(), Length(max=32)])
+    apellidos = StringField('Apellidos', validators=[DataRequired(), Length(max=32)])
+    telefono = StringField('Teléfono', validators=[DataRequired(), Length(max=16)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=64)])
+    rol_id = IntegerField('ID del Rol', validators=[DataRequired()])
+    es_propietario = BooleanField('Es Propietario')
+    
+    submit = SubmitField('Registrar')
+
+    def validate_documento(self, documento):
+        user = Usuario.query.filter_by(documento=documento.data).first()
+        if user:
+            raise ValidationError('El documento ya está registrado. Por favor, usa uno diferente.')
+
+    def validate_email(self, email):
+        user = Usuario.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('El email ya está registrado. Por favor, usa uno diferente.')
