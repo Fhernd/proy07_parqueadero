fbe90fa4ae61cd6f3cbd2747a625715409e53cbb
John Ortiz Ordoñez <johnortizo@outlook.com>
2024-11-28 05:14:40 -0500

    Esqueleto para la clase de rutas para parqueadero

---
+++ app/parqueadero_routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, g, jsonify, render_template, request
+from flask_login import login_required
+
+from app.models import MedioPago, Tarifa
+
+from app import db
+from app.routes import propietario_admin_permission
+from app.routes import todos_permiso
+
+
+class ParqueaderoRoutes:
+    """
+    Clase que gestiona las rutas de los parqueaderos.
+    """
+    def __init__(self):
+        """
+        Constructor de la clase.
+        """
+        self.blueprint = Blueprint('parqueadero', __name__)
+        self.add_routes()
+
+    def add_routes(self):
+        pass
